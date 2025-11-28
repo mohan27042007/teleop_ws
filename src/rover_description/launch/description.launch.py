fe934ea8d3e28bd44998f36dde1c783ed067c583
mohan27042007 <mail4mohan27@gmail.com>
2025-11-28 23:58:52 +0530

    first commit

--- src/rover_description/launch/description.launch.py
+++ src/rover_description/launch/description.launch.py
@@ -2,6 +2,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import Command
 from ament_index_python.packages import get_package_share_directory
+from launch_ros.parameter_descriptions import ParameterValue
 import os
 
 def generate_launch_description():
@@ -13,7 +14,13 @@ def generate_launch_description():
             package='robot_state_publisher',
             executable='robot_state_publisher',
             parameters=[{
-                'robot_description': Command(['xacro ', urdf_path])
+                'robot_description': ParameterValue(
+                    Command([
+                        'xacro ',
+                        urdf_path
+                    ]),
+                    value_type=str
+                )
             }]
         ),
         Node(
